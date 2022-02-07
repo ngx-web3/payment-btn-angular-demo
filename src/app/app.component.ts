276fa72ea24cbde04d53e80739a900e77675f603
FazioNico <contact@nicolasfazio.ch>
2022-02-07 20:03:24 +0100

    feat: add more desc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,13 @@ import '@ngx-web3/ui-payment-btn';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  amount = 250;
-  form!: FormGroup;
+  
+  public form!: FormGroup;
 
   ngOnInit() {
     this.form = new FormGroup({
       amount: new FormControl(250, Validators.required),
-      to: new FormControl('0xf9Fb1f793051fab79bc5C2AE3Fd31dD92590aEfc', Validators.required),
+      to: new FormControl('0x...', Validators.required),
       symbol: new FormControl('BNB', Validators.required)
     });
   }
