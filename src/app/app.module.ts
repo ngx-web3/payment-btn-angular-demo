80d58beb969dd4c60f27062220a3515d23cd3d73
FazioNico <contact@nicolasfazio.ch>
2022-03-17 22:48:56 +0100

    fix: use corrrect import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { PaymentPageComponent } from './payment-page/payment-page.component';
 import { DemoPageComponent } from './demo-page/demo-page.component';
 import { RouterModule } from '@angular/router';
-import { ShareComponent } from './share/share.component';
 import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PaymentPageComponent,
     DemoPageComponent,
-    ShareComponent,
     FooterComponent
   ],
   imports: [
@@ -22,7 +18,6 @@ import { FooterComponent } from './footer/footer.component';
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: DemoPageComponent },
-      { path: 'p/:code', component: PaymentPageComponent },
       { path: '**', redirectTo: '' }
     ])
   ],
